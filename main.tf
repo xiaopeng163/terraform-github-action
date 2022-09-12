2d3c181fab10c73fd357c01a62655ccc52187384
Peng Xiao <xiaoquwl@gmail.com>
2022-09-13 00:27:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
   tags = {
     Name = "my-vpc"
